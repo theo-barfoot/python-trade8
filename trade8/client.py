1646b3202e9ee24ff70ca3f0b9ff8ff3e3753c39
theo-barfoot <theodore.d.barfoot@kcl.ac.uk>
2021-02-03 18:28:41 +0000

    public endpoints complete

--- trade8/client.py
+++ trade8/client.py
@@ -13,17 +13,17 @@ class Client:
 
     def _request(self, method, endpoint, params=None, auth=False):
         url = self.API_URL + endpoint
-        url = url + '?demo=true' if self.demo else url
         if auth:
             if self.auth:
-                r = requests.request(method, url, auth=self.auth)
+                url = url + '?demo=true' if self.demo else url
+                r = requests.request(method, url, params=params, auth=self.auth)
             else:
                 print('authentication required!')  # todo: raise correct error
         else:
+            # Use public endpoints, no authentication required and demo is irrelevant
             r = requests.request(method, url, params=params)
-            print(r.url)
 
-        return r.json()
+        return r
 
     def get_positions(self):
         endpoint = '/trading/positions'
@@ -41,8 +41,10 @@ class Client:
     def update_order(self):
         pass
 
-    def close_position(self):
-        pass
+    def close_position(self, id):
+        endpoint = '/trading/trade/close'
+        params = {'id': id}
+        return self._request('post', endpoint, params, auth=True)
 
     def split_position(self):
         pass
@@ -67,13 +69,38 @@ class Client:
 
     # Public endpoints:
 
-    def get_products(self):
-        pass
+    def get_products(self, product: str = ''):
+        """
+        List products available to trade
+        :param product: (optional) Limits returned results to this product
+        :return:
+        """
+        endpoint = f'/trading/products/{product}'
+        return self._request('get', endpoint)
 
-    def get_quotes(self):
-        pass
+    def get_quotes(self, products: list):
+        """
+        Get quotes for the provided product(s)
+        :param products: list of strings of one or more products
+        :return:
+        """
+        endpoint = f"/trading/quotes/{','.join(products)}"
+        return self._request('get', endpoint)
 
-    def get_candles(self, product, resolution, **params):
+    def get_candles(self, product: str, resolution: str, **params):
+        """
+        Lists historical candles for a product. Candles returns are grouped by resolution.
+        :param product: product to get candles for
+        :param resolution: Time resolution for candle. Can be 1, 5, 15, 60, 240, or 1D.
+        :param params: Optional named query parameters
+
+        Optional query parameters:
+            signal: string of 'mid', 'bid' or 'ask'. Defaults to 'mid'
+            start: integer representing utc time in seconds after which to fetch candles
+            end: integer representing utc time in seconds before which to fetch candles
+            limit: string representing the number of candles to return, maximum is 150
+        :return:
+        """
         endpoint = f'/trading/candles/{product}/{resolution}'
         return self._request('get', endpoint, params)
 
